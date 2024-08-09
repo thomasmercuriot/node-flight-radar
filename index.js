0ca788d448053715af56f0db348cbd4908414108
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-08-10 01:51:25 +0200

    feat: add 404 and 500 error handling

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = require('express')();
+const PORT = process.env.PORT || 8080;
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from JSON' });
+  res.status(200).send({
+    message: 'Hello from JSON',
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Error handling
+app.use((req, res, next) => {
+  res.status(404).json({ message: '404 - Resource not found' });
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: '500 - Server error' });
+});
+
+// Start server
+app.listen(
+  PORT,
+  () => console.log(`Server is running on http://localhost:${PORT}`)
+);
