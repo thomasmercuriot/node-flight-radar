c6d49df503719cf186068b7823d299bb81b8bad6
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-10-14 14:43:59 +0200

    config for Heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const cors = require('cors');  // npm install cors | CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 const app = require('express')();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());  // Enable CORS for all requests. This is required to allow the front-end to access the API.
